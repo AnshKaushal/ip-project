647fb2da6a0efee8161d43faa2ed725f02876468
AnshKaushal <91865737+AnshKaushal@users.noreply.github.com>
2023-02-26 22:03:14 +0530

    Add files via upload

---
+++ pages/fun-tools.js
@@ -0,0 +1,9 @@
+const FunTools = () => {
+    return ( <>
+    <h1>Here, you'll get some fun tools</h1>
+    </> );
+}
+
+
+ 
+export default FunTools;
---
+++ pages/submit.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css';
+
+const Submit = () => {
+  return (
+    <>
+      <div className={styles.submit}>
+        <h1>Submit your photos here!</h1>
+      </div>
+    </>
+  );
+};
+
+export default Submit;
